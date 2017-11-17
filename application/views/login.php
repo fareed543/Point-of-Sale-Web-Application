8ad6302e0214efb3e99226d87235eff6a67dac36
Fareed <fareed543@gmail.com>
2017-11-17 12:43:46 +0530

    Fix Login with PIN Issue & Changed Background Desing

--- application/views/login.php
+++ application/views/login.php
@@ -13,6 +13,7 @@ $setting_keyboard = $settingData->display_keyboard;
 $setting_customer_id = $settingData->default_customer_id;
 date_default_timezone_set("$setting_timezone");
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,7 +22,6 @@ date_default_timezone_set("$setting_timezone");
         <title><?php echo $setting_site_name; ?></title>
         <link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
         <link href="<?= base_url() ?>assets/css/datepicker3.css" rel="stylesheet">
-        <link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet">
         <link href="<?= base_url() ?>assets/css/pin-login.css" rel="stylesheet">
         <script src="<?= base_url() ?>assets/js/jquery.min.js"></script>
         <!--[if lt IE 9]>
@@ -30,57 +30,69 @@ date_default_timezone_set("$setting_timezone");
         <![endif]-->
     </head>
 
-    <body style="background-color: #e1e0de;">
+    <body>
+        <div class="pincode login_with_username_form" style="display:<?php echo (!empty($alert_msg))?'block':'none'; ?>">
+            <div id="anleitung">
+                            <?php
+                            if (!empty($alert_msg)) {
+                                $flash_status = $alert_msg[0];
+                                $flash_header = $alert_msg[1];
+                                $flash_desc = $alert_msg[2];
 
-        <div class="row" id="login_with_username_form" style="display:none;">
-            <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading" style="height: 175px;">
-                        Login Page
-                        <br />
-                        <img src="<?= base_url() ?>assets/img/logo/logo.jpg" height="100px" />
-                    </div>
-                    <div class="panel-body">
-                        <form action="<?= base_url() ?>auth/login" method="post" id="loginform">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input type="hidden" name="pin" id="pin" placeholder="Pin" />
-                                    <input type="email" name="email" class="form-control" autofocus autocomplete="off" required placeholder="Email Address" />
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" name="password" class="form-control" placeholder="Password" required autocomplete="off" />
-                                </div>
-                                <br />
-                                <center>
-                                    <button class="btn btn-primary" name="sp_login">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-                                    <button class="btn btn-primary" id="login_with_pin">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login in with PIN&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-                                </center>
-                                <?php
-                                if (!empty($alert_msg)) {
-                                    $flash_status = $alert_msg[0];
-                                    $flash_header = $alert_msg[1];
-                                    $flash_desc = $alert_msg[2];
-
-                                    if ($flash_status == 'failure') {
-                                        ?>
-                                        <div class="form-group" style="text-align: center; color: #c72a25; margin-top: 15px;">
-                                            <?php echo $flash_desc; ?>
-                                        </div>
-                                        <?php
-                                    }
+                                if ($flash_status == 'failure') {
+                                    ?>
+                                    <div class="form-group warning-message" style="text-align: center; color: #c72a25; margin-top: 107px;    margin-bottom: -103px;">
+                                        <?php echo $flash_desc; ?>
+                                    </div>
+                                    <?php
                                 }
-                                ?>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div><!-- /.col-->
-        </div><!-- /.row -->	
+                            }
+                            ?>
+                        </div>
+            <div class="panel-heading">
+                Login Page
+                <br />
+                <img src="<?= base_url() ?>assets/img/logo/logo.jpg" height="100px" />
+            </div>
+            <div class="panel-body table">
+                <form action="<?= base_url() ?>auth/login" method="post" id="loginform">
+                    <fieldset>
+                        <div class="form-group">
+                            <input type="hidden" name="pin" id="pin" placeholder="Pin" />
+                            <input type="email" name="email" class="form-control" autofocus autocomplete="off" required placeholder="Email Address" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control" placeholder="Password" required autocomplete="off" />
+                        </div>
+                        <br />
+                        <center>
+                            <input type="submit" name="sp_login" class="btn btn-primary" value="Login">
+                            <input type="button" class="btn btn-primary" id="login_with_pin" value="Login in with PIN">
+                        </center>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
         <div class="pin-section">
-            <div id="pincode" class="login_with_pin_form">
+            <div class="pincode login_with_pin_form" style="display:<?php echo (!empty($alert_msg))?'none':'block'; ?>">
                 <div class="table">
                     <div class="cell">
                         <div id="anleitung">
+                            <?php
+                            if (!empty($alert_msg)) {
+                                $flash_status = $alert_msg[0];
+                                $flash_header = $alert_msg[1];
+                                $flash_desc = $alert_msg[2];
+
+                                if ($flash_status == 'failure') {
+                                    ?>
+                                    <div class="form-group warning-message" style="text-align: center; color: #c72a25; margin-top: 15px;">
+                                        <?php echo $flash_desc; ?>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                            ?>
                             <p><strong>Please enter the correct PIN-Code.</strong></p>
                         </div>
                         <div id="fields">
@@ -127,21 +139,22 @@ date_default_timezone_set("$setting_timezone");
     <script>
         jQuery(document).ready(function ($) {
             $(document).ready(function () {
-                $("#login_with_username_form").hide();
-
                 $("#login_with_username").on("click", function () {
-                    $("#login_with_username_form").show();
+                    $("#pin").val();
+                    $(".warning-message").hide();
+                    $(".login_with_username_form").show();
                     $(".login_with_pin_form").hide();
+                    return false;
                 });
-
-
                 $("#login_with_pin").on("click", function () {
+                    $("#pin").val();
+                    $(".warning-message").hide();
                     $(".login_with_pin_form").show();
-                    $("#login_with_username_form").hide();
+                    $(".login_with_username_form").hide();
+                    return false;
                 });
 
-                //var pin = (+!![] + []) + (!+[] + !![] + []) + (!+[] + !![] + !![] + []) + (!+[] + !![] + !![] + !![] + []);
-                var enterCode = "";
+               var enterCode = "";
                 enterCode.toString();
 
                 $("#numbers button").click(function () {
@@ -164,13 +177,13 @@ date_default_timezone_set("$setting_timezone");
                             },
                             error: function () {
                                 $("#fields").addClass("miss");
-                                    enterCode = "";
-                                    setTimeout(function () {
-                                        $("#fields .numberfield").removeClass("active");
-                                    }, 200);
-                                    setTimeout(function () {
-                                        $("#fields").removeClass("miss");
-                                    }, 500);
+                                enterCode = "";
+                                setTimeout(function () {
+                                    $("#fields .numberfield").removeClass("active");
+                                }, 200);
+                                setTimeout(function () {
+                                    $("#fields").removeClass("miss");
+                                }, 500);
                             },
                             dataType: 'json',
                             success: function (data) {

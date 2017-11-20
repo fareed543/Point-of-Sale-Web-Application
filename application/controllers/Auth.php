b0c722deeed2f17fb0be5fcfc110b67ee8fdf3b5
Fareed <fareed543@gmail.com>
2017-11-20 17:42:20 +0530

    Generate a Backup when user Login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -70,6 +70,11 @@ class Auth extends CI_Controller {
                         'user_role' => $role_id,
                         'user_outlet' => $out_id,
                     );
+                    
+                    /*generate database backup*/
+                    $this->exportDatabase(
+                            $this->db->hostname, $this->db->username, $this->db->password, $this->db->database, $tables = false, $backup_name = false
+                    );
 
                     $this->session->set_userdata($userdata);
 
@@ -107,6 +112,62 @@ class Auth extends CI_Controller {
         }
 
         return $ipaddress;
+    }
+
+    public function exportDatabase($host, $user, $pass, $name, $tables = false, $backup_name = false) {
+        $mysqli = new mysqli($host, $user, $pass, $name);
+        $mysqli->select_db($name);
+        $mysqli->query("SET NAMES 'utf8'");
+        $queryTables = $mysqli->query('SHOW TABLES');
+        while ($row = $queryTables->fetch_row()) {
+            $target_tables[] = $row[0];
+        }
+        if ($tables !== false) {
+            $target_tables = array_intersect($target_tables, $tables);
+        }
+        $content = "SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";\r\nSET time_zone = \"+00:00\";\r\n\r\n\r\n/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n--Database: `" . $name . "`\r\n\r\n\r\n";
+        foreach ($target_tables as $table) {
+            $result = $mysqli->query('SELECT * FROM ' . $table);
+            $fields_amount = $result->field_count;
+            $rows_num = $mysqli->affected_rows;
+            $res = $mysqli->query('SHOW CREATE TABLE ' . $table);
+            $TableMLine = $res->fetch_row();
+            $content .= "\n\n" . $TableMLine[1] . ";\n\n";
+            for ($i = 0, $st_counter = 0; $i < $fields_amount; $i++, $st_counter = 0) {
+                while ($row = $result->fetch_row()) { //when started (and every after 100 command cycle):
+                    if ($st_counter % 100 == 0 || $st_counter == 0) {
+                        $content .= "\nINSERT INTO " . $table . " VALUES";
+                    }
+                    $content .= "\n(";
+                    for ($j = 0; $j < $fields_amount; $j++) {
+                        $row[$j] = str_replace("\n", "\\n", addslashes($row[$j]));
+                        if (isset($row[$j])) {
+                            $content .= '"' . $row[$j] . '"';
+                        } else {
+                            $content .= '""';
+                        } if ($j < ($fields_amount - 1)) {
+                            $content.= ',';
+                        }
+                    }
+                    $content .=")";
+                    //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
+                    if ((($st_counter + 1) % 100 == 0 && $st_counter != 0) || $st_counter + 1 == $rows_num) {
+                        $content .= ";";
+                    } else {
+                        $content .= ",";
+                    } $st_counter = $st_counter + 1;
+                }
+            } $content .="\n\n\n";
+        }
+        $content .= "\r\n\r\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\r\n/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;\r\n/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;";
+        $backup_name = $backup_name ? $backup_name : $name . "-" . date('Y-m-d-H-i-s') . ".sql";
+        $tempFile = fopen("database/" . $backup_name, "w");
+        fwrite($tempFile, $content);
+        /* header('Content-Type: application/octet-stream');
+          header("Content-Transfer-Encoding: Binary");
+          header("Content-disposition: attachment; filename=\"" . $backup_name . "\"");
+          echo $content;
+          exit; */
     }
 
 }

20d8678ab23e669a06d52562bd54cf3e001781aa
Fareed <fareed543@gmail.com>
2018-01-05 10:35:15 +0530

    Category View/Edit/Add Template Changes

--- application/views/edit_product_category.php
+++ application/views/edit_product_category.php
@@ -12,95 +12,56 @@ if (count($categoryData) == 0) {
 $category_name = $categoryData[0]->name;
 $status = $categoryData[0]->status;
 ?>
+
+
 <link href="<?= base_url() ?>assets/plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
 <script src="<?= base_url() ?>assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
-
 <section class="content">
 	<div class="container-fluid">
-<!--<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">-->
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header"><?php echo $lang_edit_product_category; ?> : <?php echo $category_name; ?></h1>
-        </div>
-    </div><!--/.row-->
-
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-
-                    <?php
-                    if (!empty($alert_msg)) {
-                        $flash_status = $alert_msg[0];
-                        $flash_header = $alert_msg[1];
-                        $flash_desc = $alert_msg[2];
-
-                        if ($flash_status == 'failure') {
-                            ?>
-                            <div class="row" id="notificationWrp">
-                                <div class="col-md-12">
-                                    <div class="alert bg-warning" role="alert">
-                                        <i class="icono-exclamationCircle" style="color: #FFF;"></i> 
-                                        <?php echo $flash_desc; ?> <i class="icono-cross" id="closeAlert" style="cursor: pointer; color: #FFF; float: right;"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <?php
-                        }
-                        if ($flash_status == 'success') {
-                            ?>
-                            <div class="row" id="notificationWrp">
-                                <div class="col-md-12">
-                                    <div class="alert bg-success" role="alert">
-                                        <i class="icono-check" style="color: #FFF;"></i> 
-                                        <?php echo $flash_desc; ?> <i class="icono-cross" id="closeAlert" style="cursor: pointer; color: #FFF; float: right;"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <?php
-                        }
-                    }
-                    ?>
-
-
-                    <?php
-                    if ($user_role == 1) {
-                        ?>
-                        <div class="row">
-                            <div class="col-md-12" style="text-align: right;">
-                                <form action="<?= base_url() ?>products/deleteproductcategory" method="post" onsubmit="return confirm('Do you want to delete this Product Category?')">
-                                    <input type="hidden" name="cat_id" value="<?php echo $id; ?>" />
-                                    <input type="hidden" name="cat_name" value="<?php echo $category_name; ?>" />
-                                    <button type="submit" class="btn btn-primary" style="border: 0px; background-color: #c72a25;">
-                                        <?php echo $lang_delete_product_category; ?>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                        <?php
-                    }
-                    ?>
-
-                    <form action="<?= base_url() ?>products/updateProductCategory" method="post">				
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label><?php echo $lang_name; ?> <span style="color: #F00">*</span></label>
-                                    <input type="text" name="category" class="form-control" maxlength="490" autofocus required autocomplete="off" value="<?php echo $category_name; ?>" />
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label><?php echo $lang_status; ?> <span style="color: #F00">*</span></label>
-                                    <select name="status" class="form-control show-tick" data-live-search="true">
-                                        <option value="0" <?php
+		<?php echo $lang_edit_product_category; ?> : <?php echo $category_name; ?>
+		
+		<form action="<?= base_url() ?>products/updateProductCategory" method="post">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<?php if (!empty($alert_msg)) { 
+								$flash_status = $alert_msg[0];
+								$flash_header = $alert_msg[1];
+								$flash_desc = $alert_msg[2];
+							?>
+							<?php if ($flash_status == 'failure') {	?>
+								<div class="alert alert-info">
+									<strong>Heads up!</strong> <?php echo $flash_desc; ?>
+								</div>
+							<?php } ?>
+							
+							
+							<?php if ($flash_status == 'success') {?>
+								<div class="alert alert-success">
+									<strong>Well done!</strong> <?php echo $flash_desc; ?>
+								</div>
+							<?php } ?>
+							<?php } ?>
+							
+							
+							<h3 class="card-inside-title">Basic Infomation</h3>
+                            <div class="row clearfix">
+								
+								<div class="col-sm-6">
+									<div class="form-group form-float">
+										<div class="form-line">
+											<input type="text" name="category" class="form-control" maxlength="490" autofocus required autocomplete="off" value="<?php echo $category_name; ?>" />
+											<label class="form-label"><?php echo $lang_name; ?></label>
+										</div>
+									</div>
+								</div>
+								
+								
+								<div class="col-sm-6">
+									<!--<p><?php echo $lang_outlets; ?></p>-->
+									<select name="status" class="form-control show-tick" data-live-search="true">
+										  <option value="0" <?php
                                         if ($status == '0') {
                                             echo 'selected="selected"';
                                         }
@@ -110,47 +71,54 @@ $status = $categoryData[0]->status;
                                             echo 'selected="selected"';
                                         }
                                         ?>><?php echo $lang_active; ?></option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
+									</select>
+								</div>
+							</div>
+							
+							
+							<?/*?><h3 class="card-inside-title">Login Details</h3>
+							<div class="row clearfix">
+							<div class="col-sm-6">
+									<div class="form-group form-float">
+										<div class="form-line">
+											<input type="password" name="password" class="form-control" maxlength="499" autocomplete="off" value="<?php	if (!empty($alert_msg)) { echo $alert_msg[5];}	?>" />
+											<label class="form-label"><?php echo $lang_password; ?></label>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-sm-6">
+									<div class="form-group form-float">
+										<div class="form-line">
+											<input type="password" name="conpassword" class="form-control" maxlength="499" autocomplete="off" value="<?php	if (!empty($alert_msg)) {	echo $alert_msg[6];}	?>" />
+											<label class="form-label"><?php echo $lang_confirm_password; ?></label>
+										</div>
+									</div>
+								</div>
+							</div>
+							</div><?php */?>
+							
+							
+							<div class="row clearfix">
+								<div class="col-sm-6">
+									<div class="form-group">
+										<button class="btn btn-primary"><?php echo $lang_add; ?></button>
+										<a class="btn btn-primary" href="<?= base_url() ?>products/product_category"><?php echo $lang_back; ?></a>
+									</div>
+									
+									
+								</div>
+							</div>
+						</div>
+						
+					</div><!-- Panel Body // END -->
+				</div><!-- Panel Default // END -->
+			</div><!-- Col md 12 // END -->
+		</div><!-- Row // END -->
+	</form>
+	
+	
+</div>
+</div>
 
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <input type="hidden" name="id" value="<?php echo $id; ?>" />
-                                    <button class="btn btn-primary">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $lang_update; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-                                </div>
-                            </div>
-                            <div class="col-md-4"></div>
-                            <div class="col-md-4"></div>
-                        </div>
-                    </form>				
-
-
-                </div><!-- Panel Body // END -->
-            </div><!-- Panel Default // END -->
-
-            <a href="<?= base_url() ?>products/product_category" style="text-decoration: none;">
-                <div class="btn btn-success" style="background-color: #999; color: #FFF; padding: 0px 12px 0px 2px; border: 1px solid #999;"> 
-                    <i class="icono-caretLeft" style="color: #FFF;"></i><?php echo $lang_back; ?>
-                </div>
-            </a>
-
-        </div><!-- Col md 12 // END -->
-    </div><!-- Row // END -->
-
-
-    <br /><br /><br /><br /><br />
-
-<!--</div> Right Colmn // END -->
-        </div>
-</section>
-
-
-<?php
-require_once 'includes/footer.php';
-?>
+<?php require_once 'includes/footer.php'; ?>			
